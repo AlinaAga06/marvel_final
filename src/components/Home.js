81ab82ae1e3605e88ecaaab694fe0709bd13dc42
AlinaAga06 <141228720+AlinaAga06@users.noreply.github.com>
2023-09-25 16:31:18 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='homeCenter'>
-        <h2>This is a simple example of using React to Query the Marvel API!!</h2>
+        <h2>This is a  example of  React Marvel API!!</h2>
         <br></br>
-        <h3>Click on the links above to explore more!!</h3>
+        
     </div>
   )
 }
